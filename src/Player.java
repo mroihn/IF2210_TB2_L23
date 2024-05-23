6ff4576eb4aeee61116b06d911629fae8ae37db1
Jimly Nur Arif <132258178+jimlynurarif@users.noreply.github.com>
2024-05-23 10:42:51 +0700

    feat: shuffleKartu fix: Main

--- src/Player.java
+++ src/Player.java
@@ -3,98 +3,186 @@ import java.util.List;
 import java.util.Random;
 
 public class Player {
-  private List<Kartu> Deck;
-  private List<Kartu> activeDeck;
-  public Player(){
-    Deck = new ArrayList<>();
-    activeDeck = new ArrayList<>();
-  }
-  public void shuffle(){
+    private List<Kartu> deck;
+    private List<Kartu> activeDeck;
 
-  }
-  public void printDeck(){
-    for (Kartu k : Deck){
-      System.out.println(k.getName());
+    public Player(){
+        deck = new ArrayList<>();
+        activeDeck = new ArrayList<>();
     }
-  }
-  public void generateRandomCards(int numberOfCards) {
-        Deck = new ArrayList<>();
+
+    public void printDeck(){
+        System.out.println("Deck: ");
+        for (Kartu k : deck){
+            System.out.println(k.getName());
+        }
+    }
+
+    public void printActiveDeck(){
+        System.out.println("Active Deck: ");
+        for(Kartu k : activeDeck){
+            System.out.println(k.getName());
+        }
+    }
+
+    public int sizeActiveDeck(){
+        return activeDeck.size();
+    }
+
+    //bila deck kosong, do nothing
+    //bila activeDeck penuh, do nothing
+    //bila slot kosong di activeDeck kurang dari 4, shuffle kartu sejumlah slot kosong
+    //else, shuffle 4 kartu
+    // kosongAwal = jumlah slot terisi activeDeck saat pemain shuffle di awal turn
+    // return True kalau shuffle berhasil, False kalau tidak
+    public boolean shuffleKartu(int isiAwalActiveDeck, boolean isAwalTurn){
+        System.out.println("shuffleKartu running....");
+        Random random = new Random();
+        int randomNumber = random.nextInt(deck.size()); // generate probrably 0-39
+        int reserveSlotActiveDeck = 6 - activeDeck.size();
+        System.out.println("flag10");
+
+        if (isAwalTurn){
+            System.out.println("flag20");
+            if(deck.isEmpty() || reserveSlotActiveDeck == 0){
+                System.out.println("Deck kosong atau activeDeck penuh");
+                return false;
+            } else if(reserveSlotActiveDeck < 4){
+                System.out.println("flag1");
+                for(int i = 0; i < reserveSlotActiveDeck; i++){
+                    activeDeck.add(deck.get(randomNumber));
+                    deck.remove(randomNumber);
+                }
+            } else {
+                System.out.println("flag2");
+                for(int i = 0; i < 4; i++){
+                    activeDeck.add(deck.get(randomNumber));
+                    deck.remove(randomNumber);
+                }
+            }
+            
+            System.out.println("ukuran deck: " + deck.size());
+            System.out.println("ukuran activeDeck: " + activeDeck.size());
+            printDeck();
+            printActiveDeck();
+            return true;
+        }
+        else{ 
+            // bila masuk sini, berarti terjadi shuffle ulang
+            //membalikkan kondisi deck dan activeDeck seperti kondisi semula (kondisi di awal turn)
+            System.out.println("ukuran acd: ~~~~~~~~~~~~~~~~~~:" + activeDeck.size());
+            System.out.println(isiAwalActiveDeck);
+            int currentActiveDeckSize = activeDeck.size();
+            for (int j = isiAwalActiveDeck; j < currentActiveDeckSize; j++){
+                Kartu balikinKeDeck = activeDeck.removeLast();
+                deck.add(balikinKeDeck);
+                System.out.println("flag");
+            }
+            // nah, shuffle lagi
+            reserveSlotActiveDeck = 6 - activeDeck.size();
+            System.out.println("ukuran acd: ~~~~~~~~~~~~~~~~~~" + activeDeck.size());
+            System.out.println("ukuran deck: ~~~~~~~~~~~~~~~~~~" + deck.size());
+            printActiveDeck();
+            if(deck.isEmpty() || reserveSlotActiveDeck == 0){
+                System.out.println("Deck kosong atau activeDeck penuh");
+                return false;
+            } else if(reserveSlotActiveDeck < 4){
+                for(int i = 0; i < reserveSlotActiveDeck; i++){
+                    activeDeck.add(deck.get(randomNumber));
+                    deck.remove(randomNumber);
+                }
+            } else {
+                for(int i = 0; i < 4; i++){
+                    activeDeck.add(deck.get(randomNumber));
+                    deck.remove(randomNumber);
+                }
+            }
+            printDeck();
+            printActiveDeck();
+            System.out.println("ukuran deck: " + deck.size());
+            System.out.println("ukuran activeDeck: " + activeDeck.size());
+            return true;
+        }
+    }
+
+    public void generateRandomCards(int numberOfCards) {
+        deck = new ArrayList<>();
         Random random = new Random();
 
         for (int i = 0; i < numberOfCards; i++) {
             int cardType = random.nextInt(24); // Assuming 3 types of cards
             switch (cardType) {
                 case 0:
-                    Deck.add(new HiuDarat());
+                    deck.add(new HiuDarat());
                     break;
                 case 1:
-                    Deck.add(new Sapi());
+                    deck.add(new Sapi());
                     break;
                 case 2:
-                    Deck.add(new Domba());
+                    deck.add(new Domba());
                     break;
                 case 3:
-                    Deck.add(new Kuda());
+                    deck.add(new Kuda());
                     break;
                 case 4:
-                    Deck.add(new Ayam());
+                    deck.add(new Ayam());
                     break;
                 case 5:
-                    Deck.add(new Beruang());
+                    deck.add(new Beruang());
                     break;
                 case 6:
-                    Deck.add(new BijiJagung());
+                    deck.add(new BijiJagung());
                     break;
                 case 7:
-                    Deck.add(new BijiLabu());
+                    deck.add(new BijiLabu());
                     break;
                 case 8:
-                    Deck.add(new BijiStroberi());
+                    deck.add(new BijiStroberi());
                     break;
                 case 9:
-                    Deck.add(new Susu());
+                    deck.add(new Susu());
                     break;
                 case 10:
-                    Deck.add(new Telur());
+                    deck.add(new Telur());
                     break;
                 case 11:
-                    Deck.add(new SiripHiu());
+                    deck.add(new SiripHiu());
                     break;
                 case 12:
-                    Deck.add(new DagingKuda());
+                    deck.add(new DagingKuda());
                     break;
                 case 13:
-                    Deck.add(new DagingDomba());
+                    deck.add(new DagingDomba());
                     break;
                 case 14:
-                    Deck.add(new DagingBeruang());
+                    deck.add(new DagingBeruang());
                     break;
                 case 15:
-                    Deck.add(new Accelerate());
+                    deck.add(new Accelerate());
                     break;
                 case 16:
-                    Deck.add(new Delay());
+                    deck.add(new Delay());
                     break;
                 case 17:
-                    Deck.add(new Destroy());
+                    deck.add(new Destroy());
                     break;
                 case 18:
-                    Deck.add(new Protect());
+                    deck.add(new Protect());
                     break;
                 case 19:
-                    Deck.add(new Trap());
+                    deck.add(new Trap());
                     break;
                 case 20:
-                    Deck.add(new Labu());
+                    deck.add(new Labu());
                     break;
                 case 21:
-                    Deck.add(new Jagung());
+                    deck.add(new Jagung());
                     break;
                 case 22:
-                    Deck.add(new Stroberi());
+                    deck.add(new Stroberi());
                     break;
                 case 23:
-                    Deck.add(new InstantHarvest());
+                    deck.add(new InstantHarvest());
                     break;
                 default:
                     throw new IllegalStateException("Unexpected value: " + cardType);

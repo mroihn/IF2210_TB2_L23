4e243bc5c70e639f87b6202cdb61b3d654c4a26b
Jimly Nur Arif <132258178+jimlynurarif@users.noreply.github.com>
2024-05-25 21:10:37 +0700

    fix: location in txt

--- Gui/src/main/java/com/lamongan234/gui/Models/Main_Dummy.java
+++ Gui/src/main/java/com/lamongan234/gui/Models/Main_Dummy.java
@@ -3,10 +3,10 @@ package com.lamongan234.gui.Models;
 public class Main_Dummy {
     public static void main(String[] args) {
         //inisialisasi
-        Hewan hiuDarat = new HiuDarat();
-        Tanaman jagung = new BijiJagung();
-        Product daging = new DagingDomba();
-        Product tempe = new Tempe();
+        // Hewan hiuDarat = new HiuDarat();
+        // Tanaman jagung = new BijiJagung();
+        // Product daging = new DagingDomba();
+        // Product tempe = new Tempe();
 
 
         // //SECTION UNTUK SEEDING GAMEMANAGER
@@ -23,12 +23,15 @@ public class Main_Dummy {
         //save state dan lihat pada direktori Gui\src\main\java\com\lamongan234\gui\Models\tes\
         //apakah ada perubahan uang pada player1.txt
         System.out.println("isi ladang 0: "+ gameManager.getPlayer1().getLadang(0));
-        System.out.println("isi ladang 1: "+ gameManager.getPlayer1().getLadang(1));
-        gameManager.getPlayer1().moveLadangToLadang(0, 1);
+        System.out.println("isi ladang 1: "+ gameManager.getPlayer1().getLadang(3));
+        gameManager.getPlayer1().moveLadangToLadang(0, 3);
         System.out.println("isi ladang 0: "+ gameManager.getPlayer1().getLadang(0));
-        System.out.println("isi ladang 1: "+ gameManager.getPlayer1().getLadang(1));
+        System.out.println("isi ladang 1: "+ gameManager.getPlayer1().getLadang(3));
         txtSaveAndLoad.saveState(gameManager, "tes");
-
+        // System.out.println("pemisah");
+        // txtSaveAndLoad.loadState(gameManager, "tes");
+        // System.out.println("isi ladang 0: "+ gameManager.getPlayer1().getLadang(0));
+        // System.out.println("isi ladang 1: "+ gameManager.getPlayer1().getLadang(3));
 
         // //SECTION untuk mencoba save and load plugin, dalam hal ini test buat txt
         // GameManager gameManager2 = new GameManager();
@@ -80,56 +83,56 @@ public class Main_Dummy {
         // jsonPluginSaveAndLoad.saveState(gameManager, "tes");
 
         //SECTION UNTUK MANUAL MENGGUNAKAN PLAYER, tidak ada hubungan dengan GameManager di atas
-        Player p = new Player();
-        p.generateRandomCards(40);
-        p.printDeck();
-        System.out.println("hello");
-        //Test Using Items
-        Item accelerate = new Accelerate();
-        Item delay = new Delay();
-        jagung.panenKartu();
-        System.out.println("");
-        System.out.println("berat awal hiu darat: " + hiuDarat.getUmurOrBerat());
-        accelerate.Efek(hiuDarat);
-        accelerate.Efek(hiuDarat);
-        accelerate.Efek(hiuDarat);
-        System.out.println("Accelerating!!!!");
-        hiuDarat.Display();
-        System.out.println(hiuDarat.getUmurOrBerat());
-        delay.Efek(hiuDarat);
-        delay.Efek(hiuDarat);
-        delay.Efek(hiuDarat);
-        System.out.println("Delaying!!!!");
-        hiuDarat.Display();
-        System.out.println(hiuDarat.getUmurOrBerat());
-        hiuDarat.panenKartu();
-        hiuDarat.makan(daging);
-        System.out.println(hiuDarat.getUmurOrBerat());
-        hiuDarat.panenKartu();
-        hiuDarat.makan(tempe);
+        // Player p = new Player();
+        // p.generateRandomCards(40);
+        // p.printDeck();
+        // System.out.println("hello");
+        // //Test Using Items
+        // Item accelerate = new Accelerate();
+        // Item delay = new Delay();
+        // jagung.panenKartu();
+        // System.out.println("");
+        // System.out.println("berat awal hiu darat: " + hiuDarat.getUmurOrBerat());
+        // accelerate.Efek(hiuDarat);
+        // accelerate.Efek(hiuDarat);
+        // accelerate.Efek(hiuDarat);
+        // System.out.println("Accelerating!!!!");
+        // hiuDarat.Display();
+        // System.out.println(hiuDarat.getUmurOrBerat());
+        // delay.Efek(hiuDarat);
+        // delay.Efek(hiuDarat);
+        // delay.Efek(hiuDarat);
+        // System.out.println("Delaying!!!!");
+        // hiuDarat.Display();
+        // System.out.println(hiuDarat.getUmurOrBerat());
+        // hiuDarat.panenKartu();
+        // hiuDarat.makan(daging);
+        // System.out.println(hiuDarat.getUmurOrBerat());
+        // hiuDarat.panenKartu();
+        // hiuDarat.makan(tempe);
 
-        p.printDeck();
-        p.printActiveDeck();
+        // p.printDeck();
+        // p.printActiveDeck();
 
-        Toko t = new Toko();
-        System.out.println("Uang: " + p.getUang());
-        t.beli(tempe, p);
-        System.out.println("Uang: " + p.getUang());
-        t.displayToko();
+        // Toko t = new Toko();
+        // System.out.println("Uang: " + p.getUang());
+        // t.beli(tempe, p);
+        // System.out.println("Uang: " + p.getUang());
+        // t.displayToko();
 
 
-        //tes shuffle
+        // //tes shuffle
 
-        System.out.println("===========================================");
+        // System.out.println("===========================================");
 
-        p.startShuffle();
+        // p.startShuffle();
 
-        p.setLadang(jagung, 0);
-        p.printLadang();
-        System.out.println("Uang: " + p.getUang());
-        p.addUang(100);
-        System.out.println("Uang: " + p.getUang());
-        p.addToActiveDeck(delay);
+        // p.setLadang(jagung, 0);
+        // p.printLadang();
+        // System.out.println("Uang: " + p.getUang());
+        // p.addUang(100);
+        // System.out.println("Uang: " + p.getUang());
+        // p.addToActiveDeck(delay);
 
 
 
--- Gui/src/main/java/com/lamongan234/gui/Models/TxtSaveAndLoad.java
+++ Gui/src/main/java/com/lamongan234/gui/Models/TxtSaveAndLoad.java
@@ -1,3 +1,4 @@
+
 package com.lamongan234.gui.Models;
 
 import java.io.BufferedReader;
@@ -9,46 +10,35 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TxtSaveAndLoad implements SaveAndLoad {
-  private Map<String, String> DictKartu;
+  private Map<String, Kartu> DictKartu;
 
-  public TxtSaveAndLoad() {
+  public TxtSaveAndLoad(){
     DictKartu = new HashMap<>();
-    DictKartu.put("HIU_DARAT", "com.lamongan234.gui.Models.HiuDarat");
-    DictKartu.put("SAPI", "com.lamongan234.gui.Models.Sapi");
-    DictKartu.put("DOMBA", "com.lamongan234.gui.Models.Domba");
-    DictKartu.put("KUDA", "com.lamongan234.gui.Models.Kuda");
-    DictKartu.put("AYAM", "com.lamongan234.gui.Models.Ayam");
-    DictKartu.put("BERUANG", "com.lamongan234.gui.Models.Beruang");
-    DictKartu.put("LABU", "com.lamongan234.gui.Models.Labu");
-    DictKartu.put("JAGUNG", "com.lamongan234.gui.Models.Jagung");
-    DictKartu.put("STROBERI", "com.lamongan234.gui.Models.Stroberi");
-    DictKartu.put("SUSU", "com.lamongan234.gui.Models.Susu");
-    DictKartu.put("TELUR", "com.lamongan234.gui.Models.Telur");
-    DictKartu.put("SIRIP_HIU", "com.lamongan234.gui.Models.SiripHiu");
-    DictKartu.put("DAGING_DOMBA", "com.lamongan234.gui.Models.DagingDomba");
-    DictKartu.put("DAGING_KUDA", "com.lamongan234.gui.Models.DagingKuda");
-    DictKartu.put("DAGING_BERUANG", "com.lamongan234.gui.Models.DagingBeruang");
-    DictKartu.put("BIJI_LABU", "com.lamongan234.gui.Models.BijiLabu");
-    DictKartu.put("BIJI_JAGUNG", "com.lamongan234.gui.Models.BijiJagung");
-    DictKartu.put("BIJI_STROBERI", "com.lamongan234.gui.Models.BijiStroberi");
-    DictKartu.put("ACCELERATE", "com.lamongan234.gui.Models.Accelerate");
-    DictKartu.put("DELAY", "com.lamongan234.gui.Models.Delay");
-    DictKartu.put("DESTROY", "com.lamongan234.gui.Models.Destroy");
-    DictKartu.put("INSTANT_HARVEST", "com.lamongan234.gui.Models.InstantHarvest");
-    DictKartu.put("PROTECT", "com.lamongan234.gui.Models.Protect");
-    DictKartu.put("TRAP", "com.lamongan234.gui.Models.Trap");    
+    DictKartu.put("HIU_DARAT", new HiuDarat());
+    DictKartu.put("SAPI", new Sapi());
+    DictKartu.put("DOMBA", new Domba());
+    DictKartu.put("KUDA", new Kuda());
+    DictKartu.put("AYAM", new Ayam());
+    DictKartu.put("BERUANG", new Beruang());
+    DictKartu.put("LABU", new Labu());
+    DictKartu.put("JAGUNG", new Jagung());
+    DictKartu.put("STROBERI", new Stroberi());
+    DictKartu.put("SUSU", new Susu());
+    DictKartu.put("TELUR", new Telur());
+    DictKartu.put("SIRIP_HIU", new SiripHiu());
+    DictKartu.put("DAGING_DOMBA", new DagingDomba());
+    DictKartu.put("DAGING_KUDA", new DagingKuda());
+    DictKartu.put("DAGING_BERUANG", new DagingBeruang());
+    DictKartu.put("BIJI_LABU", new BijiLabu());
+    DictKartu.put("BIJI_JAGUNG", new BijiJagung());
+    DictKartu.put("BIJI_STROBERI", new BijiStroberi());
+    DictKartu.put("ACCELERATE", new Accelerate());
+    DictKartu.put("DELAY", new Delay());
+    DictKartu.put("DESTROY", new Destroy());
+    DictKartu.put("INSTANT_HARVEST", new InstantHarvest());
+    DictKartu.put("PROTECT", new Protect());
+    DictKartu.put("TRAP", new Trap());    
   }
-
-  private Kartu createInstance(String className) {
-    try {
-      Class<?> cls = Class.forName(className);
-      return (Kartu) cls.getDeclaredConstructor().newInstance();
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
-
   public void saveState(GameManager g, String fileDir) {
     fileDir = "src\\main\\java\\com\\lamongan234\\gui\\Models\\" + fileDir;
     String gamestate = fileDir + "\\gamestate.txt";
@@ -71,13 +61,18 @@ public class TxtSaveAndLoad implements SaveAndLoad {
     savePlayerState(g.getPlayer1(), player1);
     savePlayerState(g.getPlayer2(), player2);
   }
-
   private void savePlayerState(Player player, String filePath) {
     try (BufferedWriter bw = new BufferedWriter(new FileWriter(filePath))) {
+        System.out.println("Saving player state to: " + filePath);
         bw.write(String.valueOf(player.getUang()));
         bw.newLine();
         bw.write(String.valueOf(player.sizeDeck()));
         bw.newLine();
+        
+        // Debug print
+        System.out.println("Player uang: " + player.getUang());
+        System.out.println("Player deck size: " + player.sizeDeck());
+
         // Save deck state
         // for (Kartu kartu : player.getDeck()) {
         //     bw.write(kartu.getName());
@@ -86,36 +81,53 @@ public class TxtSaveAndLoad implements SaveAndLoad {
         // Save active deck state
         bw.write(String.valueOf(player.filledSlotActiveDeck()));
         bw.newLine();
-        for (int i = 0; i<player.sizeActiveDeck(); i++) {
-          if(player.getActiveDeck()[i] != null){
-            char rowChar = (char) ('A' + i);
-            bw.write(rowChar + "01" + " " + player.getActiveDeck()[i].getName().toUpperCase().replace(" ", "_"));
-            bw.newLine();
-          }
+        
+        // Debug print
+        System.out.println("Player filled active deck slots: " + player.filledSlotActiveDeck());
+        System.out.println("+++printladang");
+        player.printLadang();
+        System.out.println("+++printladang");
+
+        for (int i = 0; i < player.sizeActiveDeck(); i++) {
+            System.out.println("iterasi ke: " + i);
+            if (player.getActiveDeck()[i] != null) {
+                char rowChar = (char) ('A' + i);
+                bw.write(rowChar + "01" + " " + player.getActiveDeck()[i].getName().toUpperCase().replace(" ", "_"));
+                System.out.println("rowChar: " + rowChar + "01" + " " + player.getActiveDeck()[i].getName().toUpperCase().replace(" ", "_"));
+                bw.newLine();
+
+                // Debug print
+                System.out.println("Active deck card: " + player.getActiveDeck()[i].getName());
+            }
         }
         // Save ladang state
         bw.write(String.valueOf(player.jumlahSlotLadangTerisi()));
         bw.newLine();
-        for (int i =0; i < player.getLadang().length; i ++) {
-          if(player.getLadang()[i] != null){
-            int row = i/5;
-            // int col = i - row;
-            char colNumber = (char) ('A' + row);
-  
-            // Format column index to start from 1
-            String rowLetter = String.format("%02d", row + 1);
-            bw.write(colNumber + rowLetter + " " + player.getLadang()[i].getName().toUpperCase().replace(" ", "_") + " " +
-            player.getLadang()[i].getUmurOrBerat() + " " + player.getLadang()[i].getListAppliedItem().size());
-            for (Map.Entry<String, Integer> entry : player.getLadang()[i].getListAppliedItem().entrySet()) {
-              bw.write(" " + entry.getKey().toUpperCase().replace(" ", "_"));
+        
+        // Debug print
+        System.out.println("Player filled ladang slots: " + player.jumlahSlotLadangTerisi());
+
+        for (int i = 0; i < player.getLadang().length; i++) {
+            if (player.getLadang()[i] != null) {
+                int row = i % 5;
+                int col = i /5 ;
+                char colNumber = (char) ('A' + row);
+                String rowLetter = String.format("%02d", col + 1);
+                bw.write(colNumber + rowLetter + " " + player.getLadang()[i].getName().toUpperCase().replace(" ", "_") + " " +
+                        player.getLadang()[i].getUmurOrBerat() + " " + player.getLadang()[i].getListAppliedItem().size());
+                for (Map.Entry<String, Integer> entry : player.getLadang()[i].getListAppliedItem().entrySet()) {
+                    bw.write(" " + entry.getKey().toUpperCase().replace(" ", "_"));
+                }
+                bw.newLine();
+                
+                // Debug print
+                System.out.println("Ladang card: " + player.getLadang()[i].getName());
             }
-            bw.newLine();
-          }
         }
     } catch (IOException e) {
-      e.printStackTrace();
+        e.printStackTrace();
     }
-  }
+}
 
   public void loadState(GameManager g, String fileDir){
     fileDir = "src\\main\\java\\com\\lamongan234\\gui\\Models\\" + fileDir;
@@ -138,7 +150,7 @@ public class TxtSaveAndLoad implements SaveAndLoad {
             String itemName = parts[0];
             int itemQuantity = Integer.parseInt(parts[1]);
             for(int i=0; i<itemQuantity; i++){
-              Product p = (Product) createInstance(DictKartu.get(itemName));
+              Product p = (Product) DictKartu.get(itemName);
               toko.AddKartu(p);
             }
           }
@@ -170,7 +182,7 @@ public class TxtSaveAndLoad implements SaveAndLoad {
           if (parts.length == 2) {
             String position = parts[0];
             String itemName = parts[1];
-            Kartu card = createInstance(DictKartu.get(itemName));
+            Kartu card = DictKartu.get(itemName);
             p.addToActiveDeck(card);
           }
         }
@@ -186,24 +198,26 @@ public class TxtSaveAndLoad implements SaveAndLoad {
             String position = parts[0];
             int col = position.charAt(0) - 'A';
             int row = Integer.parseInt(position.substring(1)) - 1;
+            System.out.println("coordinates" + row + " " + col);
             int pos = row*5 + col;
             String itemName = parts[1];
             int umur_or_berat = Integer.parseInt(parts[2]);
             int appliedItems = Integer.parseInt(parts[3]);
-            Kartu card = createInstance(DictKartu.get(itemName));
+            Kartu card = DictKartu.get(itemName);
             if(card instanceof Hewan){
               Hewan hewan = (Hewan) card;
               hewan.setBeratOrUmur(umur_or_berat);
               for(int i = 0; i< appliedItems; i++){
-                Item item = (Item) createInstance(DictKartu.get(parts[4+i]));
+                Item item = (Item) DictKartu.get(parts[4+i]);
                 hewan.AppliedItem(item);
               }
+              System.out.println("flag1");
               p.setLadang(hewan, pos);
             }else{
               Tanaman tanaman = (Tanaman) card;
               tanaman.setBeratOrUmur(umur_or_berat);
               for(int i = 0; i< appliedItems; i++){
-                Item item = (Item) createInstance(DictKartu.get(parts[4+i]));
+                Item item = (Item) DictKartu.get(parts[4+i]);
                 tanaman.AppliedItem(item);
               }
               p.setLadang(tanaman, pos);
@@ -238,7 +252,7 @@ public class TxtSaveAndLoad implements SaveAndLoad {
           if (parts.length == 2) {
             String position = parts[0];
             String itemName = parts[1];
-            Kartu card = createInstance(DictKartu.get(itemName));
+            Kartu card = DictKartu.get(itemName);
             p.addToActiveDeck(card);
           }
         }
@@ -258,12 +272,12 @@ public class TxtSaveAndLoad implements SaveAndLoad {
             String itemName = parts[1];
             int umur_or_berat = Integer.parseInt(parts[2]);
             int appliedItems = Integer.parseInt(parts[3]);
-            Kartu card = createInstance(DictKartu.get(itemName));
+            Kartu card = DictKartu.get(itemName);
             if(card instanceof Hewan){
               Hewan hewan = (Hewan) card;
               hewan.setBeratOrUmur(umur_or_berat);
               for(int i = 0; i< appliedItems; i++){
-                Item item = (Item) createInstance(DictKartu.get(parts[4+i]));
+                Item item = (Item) DictKartu.get(parts[4+i]);
                 hewan.AppliedItem(item);
               }
               p.setLadang(hewan, pos);
@@ -271,7 +285,7 @@ public class TxtSaveAndLoad implements SaveAndLoad {
               Tanaman tanaman = (Tanaman) card;
               tanaman.setBeratOrUmur(umur_or_berat);
               for(int i = 0; i< appliedItems; i++){
-                Item item = (Item) createInstance(DictKartu.get(parts[4+i]));
+                Item item = (Item) DictKartu.get(parts[4+i]);
                 tanaman.AppliedItem(item);
               }
               p.setLadang(tanaman, pos);
